df77e8781a59113f54ef338dd775f6a6ac16a218
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2020-01-30 15:05:22 +0100

    Add betterlockscreen for Linux

--- install.sh
+++ install.sh
@@ -178,6 +178,7 @@ case "$(uname)" in
 
     yay -Syu --noconfirm --needed \
         awslogs \
+        betterlockscreen \
         cloudfoundry-cli \
         dropbox \
         golangci-lint \
@@ -221,6 +222,10 @@ case "$(uname)" in
     
     # Set colors for pacman
     sudo sed -i 's/#Color/Color/' /etc/pacman.conf
+
+    # Configure betterlockscreen
+    betterlockscreen --update ~/.config/wallpapers/small-memory.jpg
+    systemctl enable "betterlockscreen@$USER"
     ;;
 
 # Default
