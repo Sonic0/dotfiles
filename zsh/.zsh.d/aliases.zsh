6caebe72247deb1ba317d563662cf01e68d28721
Andrea Salvatori <andrea.salvatori92@gmail.com>
2020-11-19 23:52:12 +0100

    aliases.zsh streamlining

--- zsh/.zsh.d/aliases.zsh
+++ zsh/.zsh.d/aliases.zsh
@@ -6,15 +6,8 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
-# Git aliases
-alias glog='git log --log-size --decorate=full'
-
 # General
 alias diff='diff --color=auto'
 alias grep='grep --color=auto'
 alias ll='ls -alFh --color=auto'
 alias exa='exa -abghHliS --group-directories-first'
-alias e='exa'
-alias c='clear'
-alias less='lnav'
-alias cat='bat'
