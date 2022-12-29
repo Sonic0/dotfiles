67fa1d607b0736c684e541f7b63151b882bb41d0
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2022-12-29 23:26:57 +0100

    Simplify Neovim setup by using Lazy and Mason

--- install.sh
+++ install.sh
@@ -31,48 +31,48 @@ case "${DISTRO:-OS}" in
         git clone https://github.com/Sonic0/dotfiles.git ~/dotfiles
     fi
 
-    # Install Stow if not installed
-    sudo pacman -S --noconfirm --needed stow
-    # Remove existing config files
-    if [ -f ~/.zshrc ]; then
-        rm ~/.zshrc
-    fi
-    if [ -f ~/.zshenv ]; then
-        rm ~/.zshenv
-    fi
-    # Stow subdirectories of dotfiles
-    printf '\e[1mLinking dotfiles to your home directory\e[0m\n'
-    for dir in ~/dotfiles/*/; do
-        stow --dir ~/dotfiles --target ~ "$(basename "${dir}")"
-    done
-    sudo pacman -Rns --noconfirm stow
+	# Install Stow if not installed
+	sudo pacman -S --noconfirm --needed stow
+	# Remove existing config files
+	if [ -f ~/.zshrc ]; then
+		rm ~/.zshrc
+	fi
+	if [ -f ~/.zshenv ]; then
+		rm ~/.zshenv
+	fi
+	# Stow subdirectories of dotfiles
+	printf '\e[1mLinking dotfiles to your home directory\e[0m\n'
+	for dir in ~/dotfiles/*/; do
+		stow --dir ~/dotfiles --target ~ "$(basename "${dir}")"
+	done
+	sudo pacman -Rns --noconfirm stow
 
-    # Install Paru if not installed
-    if [ ! -x "$(command -v paru)" ]; then
-        printf '\e[1mInstalling Paru\e[0m\n'
-        git clone https://aur.archlinux.org/paru-bin.git /tmp/paru
-        (cd /tmp/paru && makepkg -si)
-    fi
+	# Install Paru if not installed
+	if [ ! -x "$(command -v paru)" ]; then
+		printf '\e[1mInstalling Paru\e[0m\n'
+		git clone https://aur.archlinux.org/paru-bin.git /tmp/paru
+		(cd /tmp/paru && makepkg -si)
+	fi
 
-    # Set colors for pacman
-    sudo sed -i 's/#Color/Color/' /etc/pacman.conf
+	# Set colors for pacman
+	sudo sed -i 's/#Color/Color/' /etc/pacman.conf
 
-    # Install Pacmanfile if not installed
-    if [ ! -x "$(command -v pacmanfile)" ]; then
-        printf '\e[1mInstalling Pacmanfile\e[0m\n'
-        paru -S --noconfirm --needed pacmanfile
-    fi
+	# Install Pacmanfile if not installed
+	if [ ! -x "$(command -v pacmanfile)" ]; then
+		printf '\e[1mInstalling Pacmanfile\e[0m\n'
+		paru -S --noconfirm --needed pacmanfile
+	fi
 
-    # Install packages using Pacmanfile
-    printf '\e[1mInstalling desired packages using Pacmanfile\e[0m\n'
-    pacmanfile sync --noconfirm
+	# Install packages using Pacmanfile
+	printf '\e[1mInstalling desired packages using Pacmanfile\e[0m\n'
+	pacmanfile sync --noconfirm
 
-    # Change npm folder
-    if [ -x "$(command -v npm)" ]; then
-        mkdir -p ~/.node_modules/lib
-        npm config set prefix '~/.node_modules'
-    fi
-    ;;
+	# Change npm folder
+	if [ -x "$(command -v npm)" ]; then
+		mkdir -p ~/.node_modules/lib
+		npm config set prefix '~/.node_modules'
+	fi
+	;;
 
 'Ubuntu')
     # Install Git if not installed
@@ -176,31 +176,31 @@ case "${DISTRO:-OS}" in
 
 'Darwin')
 
-    # Install Xcode Command Line Tools if not installed
-    if ! xcode-select -p >/dev/null; then
-        printf 'Xcode Command Line Tools not installed. You will have to run the script again after successfully installing them. Install now? (Y/n)'
-        read -r
-        echo
-        if ! "$REPLY" | grep -Eq '^[Nn]'; then
-            xcode-select --install
-            echo 'Please run the script again after the installation has finished'
-        else
-            echo 'Please install the Xcode Command Line Tools and run then script again.'
-        fi
-        exit
-    fi
+	# Install Xcode Command Line Tools if not installed
+	if ! xcode-select -p >/dev/null; then
+		printf 'Xcode Command Line Tools not installed. You will have to run the script again after successfully installing them. Install now? (Y/n)'
+		read -r
+		echo
+		if ! "$REPLY" | grep -Eq '^[Nn]'; then
+			xcode-select --install
+			echo 'Please run the script again after the installation has finished'
+		else
+			echo 'Please install the Xcode Command Line Tools and run then script again.'
+		fi
+		exit
+	fi
 
-    # Install Homebrew if not installed
-    if [ ! -x "$(command -v brew)" ]; then
-        printf '\e[1mInstalling Homebrew\e[0m\n'
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    fi
+	# Install Homebrew if not installed
+	if [ ! -x "$(command -v brew)" ]; then
+		printf '\e[1mInstalling Homebrew\e[0m\n'
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
 
-    # Install git if not installed
-    if [ ! -x "$(command -v git)" ]; then
-        printf '\e[1mInstalling Git\e[0m\n'
-        brew install git
-    fi
+	# Install git if not installed
+	if [ ! -x "$(command -v git)" ]; then
+		printf '\e[1mInstalling Git\e[0m\n'
+		brew install git
+	fi
 
     # git clone these dotfiles if not done yet
     if [ ! -d ~/dotfiles ]; then
@@ -208,27 +208,27 @@ case "${DISTRO:-OS}" in
         git clone https://github.com/Sonic0/dotfiles.git ~/dotfiles
     fi
 
-    # Install Stow if not installed
-    if [ ! -x "$(command -v stow)" ]; then
-        brew install stow
-    fi
-    # Remove existing config files
-    if [ -f ~/.zshrc ]; then
-        rm ~/.zshrc
-    fi
-    # Stow subdirectories of dotfiles
-    printf '\e[1mLinking dotfiles to your home directory\e[0m\n'
-    for dir in ~/dotfiles/*/; do
-        stow --dir ~/dotfiles --target ~ "$(basename "${dir}")"
-    done
-    # Remove Stow
-    brew uninstall stow
+	# Install Stow if not installed
+	if [ ! -x "$(command -v stow)" ]; then
+		brew install stow
+	fi
+	# Remove existing config files
+	if [ -f ~/.zshrc ]; then
+		rm ~/.zshrc
+	fi
+	# Stow subdirectories of dotfiles
+	printf '\e[1mLinking dotfiles to your home directory\e[0m\n'
+	for dir in ~/dotfiles/*/; do
+		stow --dir ~/dotfiles --target ~ "$(basename "${dir}")"
+	done
+	# Remove Stow
+	brew uninstall stow
 
-    # Install packages using Brewfile
-    printf '\e[1mInstalling desired packages using Pacmanfile\e[0m\n'
-    brew update
-    brew upgrade
-    cat ~/.config/homebrew/*Brewfile | brew bundle --file=-
+	# Install packages using Brewfile
+	printf '\e[1mInstalling desired packages using Pacmanfile\e[0m\n'
+	brew update
+	brew upgrade
+	cat ~/.config/homebrew/*Brewfile | brew bundle --file=-
 
     # Install oh-my-zsh
     if [ ! -d ~/.oh-my-zsh ]; then
@@ -238,24 +238,18 @@ case "${DISTRO:-OS}" in
     # Install the Python Neovim package
     pip3 install --upgrade --user pynvim
 
-    # Install additional language servers currently not available via Homebrew
-    npm install --global \
-        typescript-language-server \
-        svelte-language-server \
-        eslint_d
+	# Install additional Go tooling currently not available via Homebrew
+	go install golang.org/x/tools/cmd/goimports@latest
 
-    # Install additional Go tooling currently not available via Homebrew
-    go install golang.org/x/tools/cmd/goimports@latest
-
-    # Set dark mode
-    sudo defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Dark
-    ;;
+	# Set dark mode
+	sudo defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Dark
+	;;
 
 # Default
 *)
-    printf '\e[1mOS not supported for automated setup. Please install manually.\e[0m\n'
-    exit 1
-    ;;
+	printf '\e[1mOS not supported for automated setup. Please install manually.\e[0m\n'
+	exit 1
+	;;
 esac
 
 # Install Neovim plugin manager
@@ -300,9 +294,9 @@ fi
 
 # Use zsh
 if [ -x "$(command -v zsh)" ] && [ "$SHELL" != "$(command -v zsh)" ]; then
-    printf '\e[1mChanging your shell to zsh\e[0m\n'
-    grep -q -F "$(command -v zsh)" /etc/shells || sudo sh -c 'echo "$(command -v zsh)" >> /etc/shells'
-    chsh -s "$(command -v zsh)"
+	printf '\e[1mChanging your shell to zsh\e[0m\n'
+	grep -q -F "$(command -v zsh)" /etc/shells || sudo sh -c 'echo "$(command -v zsh)" >> /etc/shells'
+	chsh -s "$(command -v zsh)"
 fi
 
 # Remove existing bash config files
