9dca38e9a3714b341c5c266017da8cdd208aa357
Andrea Salvatori <16443598+Sonic0@users.noreply.github.com>
2024-08-06 00:10:40 +0200

    Update zsh aliases

--- zsh/.zsh.d/aliases.zsh
+++ zsh/.zsh.d/aliases.zsh
@@ -8,3 +8,8 @@ alias diff='diff --color=auto'
 alias grep='grep --color=auto'
 alias ls='eza'
 alias eza='eza -abghHliS --group-directories-first'
+alias cat='batcat'
+alias bathelp='batcat --plain --language=help'
+help() {
+    "$@" --help 2>&1 | bathelp
+}
